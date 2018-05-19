e3ba514315528010f6681789806c1cac4d228650
David Cunningham <david7789@gmail.com>
2018-05-19 19:51:23 -0400

    Changed backup photo.

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -5,7 +5,7 @@
   $('body')
     .vide({
         mp4: "http://cdn.bglad.io/vid/stock/timelapse-busy-place.mp4",
-        poster: "https://cdn.britannica.com/668x448/11/91611-004-C997AF25.jpg",
+        poster: "http://cdn.bglad.io/img/stock/antwerp-central-station.jpg",
       }, {
         posterType: 'jpg',
         playbackRate: 4,
@@ -13,4 +13,3 @@
       });
 
 })(jQuery); // End of use strict
-//<iframe class="embed-responsive embed-responsive-16by9" width="560" height="315" src="https://www.youtube.com/embed/J7F6KVWzjr8?autohide=1&amp;showinfo=0" frameborder="0" allowfullscreen=""></iframe>
