ff2e79d31bab96cc639b3e183d3241a40941a858
David Cunningham <david7789@gmail.com>
2018-05-19 15:58:23 -0400

    LastWeekTicket.com - initial commit.

---
+++ js/coming-soon.js
@@ -0,0 +1,14 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "https://archive.org/download/ElRobotTheRobot/Robocito_512kb.mp4",
+    poster: "https://cdn.britannica.com/668x448/11/91611-004-C997AF25.jpg",
+  }, {
+    posterType: 'jpg',
+    playbackRate: 0.7,
+    //muted: false
+  });
+
+})(jQuery); // End of use strict
